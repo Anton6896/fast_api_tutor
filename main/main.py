89cb0edf25a64663a41adf753d38a4529e127dc3
anton <anton-r@live.com>
2021-06-07 21:27:18 +0300

    line reading

--- main/main.py
+++ main/main.py
@@ -1,9 +1,34 @@
-from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def index():
+    return {"data": {
+        "blog_name": "new blog"
+    }}
+
+
+
+@app.get("/blog/{id}")
+def show_blog(id: int):
+    """
+    any /blog/String will be error from this point
+    reading line by line
+    """
+    return {"data": {
+        "id": id
+    }}
+
+
+dummy_comments = {
+    1: "comment1", 2: "comment2", 3: "comment3",
+}
+
+
+@app.get("/blog/{id}/comment")
+def show_blog(id: int):
+    return {"data": {
+        "comments": dummy_comments.get(id)
+    }}
